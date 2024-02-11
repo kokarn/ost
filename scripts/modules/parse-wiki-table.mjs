4e00a2c95abde2bf9440f8188ea69731ed1f4ed9
Oskar Risberg <oskar.risberg@stendahls.se>
2024-02-11 22:26:04 +0100

    DRY

--- scripts/modules/parse-wiki-table.mjs
+++ scripts/modules/parse-wiki-table.mjs
@@ -1,53 +1,58 @@
-const parseWikiTable = async ($, keys, tableIndex = false, tableKey = false, nameSuffix = false, tableKeyIndex = 0) => {
-    const crafts = [];
-    if(tableIndex){
-        $('table').eq(tableIndex).find('tbody').find('tr').each((outerIndex, outerElement) => {
-            const craft = {};
+const parseRow = ($, outerElement, keys, crafts, nameSuffix) => {
+    const craft = {};
 
-            $(outerElement).find('td').each((i, el) => {
-                if($(el).find('br').length > 0) {
-                    craft[keys[i]] = [];
+    $(outerElement).find('td').each((i, el) => {
+        if($(el).find('br').length > 0) {
+            craft[keys[i]] = [];
 
-                    $(el).find('a').each((itemIndex, itemElement) => {
-                        craft[keys[i]].push($(itemElement).text().trim());
-                    });
-
-                    craft[keys[i]] = craft[keys[i]].filter(Boolean);
-                } else {
-                    const contents = $(el).text().trim();
-
-                    craft[keys[i]] = contents;
-                }
+            $(el).find('a').each((itemIndex, itemElement) => {
+                craft[keys[i]].push($(itemElement).text().trim());
             });
 
-            if(nameSuffix){
-                craft.resultName = `${craft.resultName}${nameSuffix}`;
-            }
+            craft[keys[i]] = craft[keys[i]].filter(Boolean);
+        } else {
+            const contents = $(el).text().trim();
 
-            if(craft.wikiMaterial1 || craft.wikiMaterial2 || craft.wikiMaterial3){
-                craft.materials = [];
-                if(Array.isArray(craft.wikiMaterial1)){
-                    craft.materials.concat(craft.wikiMaterial1);
-                } else {
-                    craft.materials.push(craft.wikiMaterial1);
-                }
+            craft[keys[i]] = contents;
+        }
+    });
 
-                if(Array.isArray(craft.wikiMaterial2)){
-                    craft.materials.concat(craft.wikiMaterial2);
-                } else {
-                    craft.materials.push(craft.wikiMaterial2);
-                }
+    if(nameSuffix){
+        craft.resultName = `${craft.resultName}${nameSuffix}`;
+    }
 
-                if(Array.isArray(craft.wikiMaterial3)){
-                    craft.materials.concat(craft.wikiMaterial3);
-                } else {
-                    craft.materials.push(craft.wikiMaterial3);
-                }
-            }
+    if(craft.wikiMaterial1 || craft.wikiMaterial2 || craft.wikiMaterial3){
+        craft.materials = [];
+        if(Array.isArray(craft.wikiMaterial1)){
+            craft.materials.concat(craft.wikiMaterial1);
+        } else {
+            craft.materials.push(craft.wikiMaterial1);
+        }
 
-            crafts.push(craft);
+        if(Array.isArray(craft.wikiMaterial2)){
+            craft.materials.concat(craft.wikiMaterial2);
+        } else {
+            craft.materials.push(craft.wikiMaterial2);
+        }
+
+        if(Array.isArray(craft.wikiMaterial3)){
+            craft.materials.concat(craft.wikiMaterial3);
+        } else {
+            craft.materials.push(craft.wikiMaterial3);
+        }
+    }
+
+    crafts.push(craft);
+}
+
+const parseWikiTable = async ($, keys, tableIndex = false, tableKey = false, nameSuffix = false, tableKeyIndex = 0) => {
+    const crafts = [];
+    if(typeof tableIndex === 'number'){
+        $('table').eq(tableIndex).find('tbody').find('tr').each((outerIndex, outerElement) => {
+            parseRow($, outerElement, keys, crafts, nameSuffix);
         });
     } else if(tableKey){
+        console.log('Parsing by table key');
         $('table').each((index, tableElement) => {
             const firstColumnKey = $(tableElement).find('th').eq(tableKeyIndex).text().trim().toLowerCase();
 
@@ -56,52 +61,7 @@ const parseWikiTable = async ($, keys, tableIndex = false, tableKey = false, nam
             }
 
             $(tableElement).find('tbody').find('tr').each((outerIndex, outerElement) => {
-                const craft = {};
-
-                $(outerElement).find('td').each((i, el) => {
-                    if($(el).find('br').length > 0) {
-                        craft[keys[i]] = [];
-
-                        $(el).find('a').each((itemIndex, itemElement) => {
-                            craft[keys[i]].push($(itemElement).text().trim());
-                        });
-
-                        craft[keys[i]] = craft[keys[i]].filter(Boolean);
-                    } else {
-                        const contents = $(el).text().trim();
-
-                        // console.log(contents);
-
-                        craft[keys[i]] = contents;
-                    }
-                });
-
-                if(nameSuffix){
-                    craft.resultName = `${craft.resultName}${nameSuffix}`;
-                }
-
-                if(craft.wikiMaterial1 || craft.wikiMaterial2 || craft.wikiMaterial3){
-                    craft.materials = [];
-                    if(Array.isArray(craft.wikiMaterial1)){
-                        craft.materials.concat(craft.wikiMaterial1);
-                    } else {
-                        craft.materials.push(craft.wikiMaterial1);
-                    }
-
-                    if(Array.isArray(craft.wikiMaterial2)){
-                        craft.materials.concat(craft.wikiMaterial2);
-                    } else {
-                        craft.materials.push(craft.wikiMaterial2);
-                    }
-
-                    if(Array.isArray(craft.wikiMaterial3)){
-                        craft.materials.concat(craft.wikiMaterial3);
-                    } else {
-                        craft.materials.push(craft.wikiMaterial3);
-                    }
-                }
-
-                crafts.push(craft);
+                parseRow($, outerElement, keys, crafts, nameSuffix);
             });
         });
     }
