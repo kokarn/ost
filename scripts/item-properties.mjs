5f504d0a97f120c8ca6397d7e3d5fcd7606f1c7a
Oskar Risberg <oskar.risberg@stendahls.se>
2024-03-04 08:03:17 +0100

    use osrsbox

--- scripts/item-properties.mjs
+++ scripts/item-properties.mjs
@@ -13,46 +13,47 @@ const data = JSON.parse(fs.readFileSync('data/mapping.json', 'utf8'));
 
 const currentData = JSON.parse(fs.readFileSync(join(__dirname, '../src/data/item-properties-raw.json'), 'utf8'));
 
-let itemData = {};
+const updatedData = await got('https://raw.githubusercontent.com/0xNeffarion/osrsreboxed-db/master/docs/items-complete.json');
+const itemData = JSON.parse(updatedData.body);
 
-let index = 1;
-for(const itemId in data){
-    console.log(`Loading item ${index} of ${Object.keys(data).length}`);
-    index = index + 1;
+// let index = 1;
+// for(const itemId in data){
+//     console.log(`Loading item ${index} of ${Object.keys(data).length}`);
+//     index = index + 1;
 
-    if(currentData[itemId]){
-        itemData[itemId] = currentData[itemId];
-        continue;
-    };
+//     if(currentData[itemId]){
+//         itemData[itemId] = currentData[itemId];
+//         continue;
+//     };
 
-    const url = `https://oldschool.runescape.wiki/w/Special:Lookup?type=item&id=${itemId}`;
-    const pageData = await got(url);
+//     const url = `https://oldschool.runescape.wiki/w/Special:Lookup?type=item&id=${itemId}`;
+//     const pageData = await got(url);
 
-    const $ = cheerio.load(pageData.body);
+//     const $ = cheerio.load(pageData.body);
 
-    itemData[itemId] = {};
+//     itemData[itemId] = {};
 
-    $('.infobox-item tr').each((i, el) => {
-        const key = $(el).find('th').eq(0).text().trim();
-        let value = $(el).find('td').eq(0).text().trim();
+//     $('.infobox-item tr').each((i, el) => {
+//         const key = $(el).find('th').eq(0).text().trim();
+//         let value = $(el).find('td').eq(0).text().trim();
 
-        if(!key || !value) {
-            return true;
-        }
+//         if(!key || !value) {
+//             return true;
+//         }
 
-        if(typeof value === 'string' && value.toLowerCase() === 'yes'){
-            value = true;
-        }
+//         if(typeof value === 'string' && value.toLowerCase() === 'yes'){
+//             value = true;
+//         }
 
-        if(typeof value === 'string' && value.toLowerCase() === 'no'){
-            value = false;
-        }
+//         if(typeof value === 'string' && value.toLowerCase() === 'no'){
+//             value = false;
+//         }
 
-        itemData[itemId][camelCase(key)] = value;
-    });
+//         itemData[itemId][camelCase(key)] = value;
+//     });
 
-    fs.writeFileSync(join(__dirname, '..', 'src', 'data', 'item-properties-raw.json'), JSON.stringify(itemData, null, 4));
-}
+//     fs.writeFileSync(join(__dirname, '..', 'src', 'data', 'item-properties-raw.json'), JSON.stringify(itemData, null, 4));
+// }
 
 let minimalItemData = {};
 
