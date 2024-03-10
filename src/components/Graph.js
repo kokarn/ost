4a4dfb4a28985d301a35792d13a3e19e9b32c90f
Oskar Risberg <oskar.risberg@stendahls.se>
2024-03-10 15:29:04 +0100

    Start working on ranges

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import { LineChart } from '@mui/x-charts/LineChart';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 import numberFormat from '../modules/number-format.mjs';
 import loadJSON from '../modules/load-json.mjs';
@@ -8,9 +10,45 @@ export default function Graph({itemId}) {
     const [xData, setXData] = useState([]);
     const [lowData, setLowData] = useState([]);
     const [highData, setHighData] = useState([]);
-    const [historicalDays] = useState(1);
+    const [historicalDays, setHistoricalDays] = useState(1);
     const [scaleMax, setScaleMax] = useState(0);
     const [scaleMin, setScaleMin] = useState(0);
+    const [localMax, setLocalMax] = useState(0);
+    const [localMin, setLocalMin] = useState(0);
+
+    const handleChange = (event, newDateRange) => {
+        setHistoricalDays(newDateRange);
+    };
+
+    const children = [
+        <ToggleButton
+            value={1}
+            key="left"
+        >
+            24h
+        </ToggleButton>,
+        <ToggleButton
+            value={7}
+            key="center"
+        >
+            7d
+        </ToggleButton>,
+        <ToggleButton
+            value={30}
+            key="right"
+        >
+            30d
+        </ToggleButton>,
+        // <ToggleButton value="justify" key="justify">
+        //   <FormatAlignJustifyIcon />
+        // </ToggleButton>,
+    ];
+
+    const control = {
+        value: historicalDays,
+        onChange: handleChange,
+        exclusive: true,
+    };
 
     useEffect(() => {
         const loadData = async () => {
@@ -38,6 +76,8 @@ export default function Graph({itemId}) {
                 const localMax = Math.max(...highData.filter((value) => value !== null));
                 const localMin = Math.min(...lowData.filter((value) => value !== null));
 
+                setLocalMax(localMax);
+                setLocalMin(localMin);
                 setScaleMax(localMax * 1.1);
                 setScaleMin(localMin * 0.9);
             }
@@ -47,37 +87,47 @@ export default function Graph({itemId}) {
     }, [itemId, historicalDays]);
 
     return (
-        <LineChart
-            xAxis={[{
-                data: xData,
-                scaleType: 'time',
-                // tickMinStep: 3600 * 1000 * ,
-                valueFormatter: (value) => new Date(value).toLocaleTimeString(),
-            }]}
-            yAxis={[{
-                scaleType: 'linear',
-                valueFormatter: numberFormat,
-                max: scaleMax,
-                min: scaleMin,
-            }]}
-            series={[
-                {
-                    data: lowData,
-                    label: 'Low Price',
-                    connectNulls: true,
-                    showMark: ({ index }) => index % 10 === 0,
-                },
-                {
-                    data: highData,
-                    label: 'High Price',
-                    connectNulls: true,
-                    showMark: ({ index }) => index % 10 === 0,
-                }
-            ]}
-            grid={{
-                horizontal: true,
-            }}
-            height={300}
-        />
+        <div>
+            <ToggleButtonGroup
+                size="small"
+                {...control}
+                aria-label="Small sizes"
+            >
+                {children}
+            </ToggleButtonGroup>
+            Min: {numberFormat(localMin)} Max: {numberFormat(localMax)}
+            <LineChart
+                xAxis={[{
+                    data: xData,
+                    scaleType: 'time',
+                    // tickMinStep: 3600 * 1000 * ,
+                    valueFormatter: (value) => new Date(value).toLocaleTimeString(),
+                }]}
+                yAxis={[{
+                    scaleType: 'linear',
+                    valueFormatter: numberFormat,
+                    max: scaleMax,
+                    min: scaleMin,
+                }]}
+                series={[
+                    {
+                        data: lowData,
+                        label: 'Low Price',
+                        connectNulls: true,
+                        showMark: ({ index }) => index % 10 === 0,
+                    },
+                    {
+                        data: highData,
+                        label: 'High Price',
+                        connectNulls: true,
+                        showMark: ({ index }) => index % 10 === 0,
+                    }
+                ]}
+                grid={{
+                    horizontal: true,
+                }}
+                height={300}
+            />
+        </div>
     );
 }
