0ef6f5fcc990c293f0f18eab52c0dcba994bec2e
Oskar Risberg <oskar.risberg@stendahls.se>
2024-03-13 19:57:33 +0100

    improve font loading

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Runescape from './fonts/runescape.woff2';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
@@ -41,17 +40,6 @@ const theme = createTheme({
         },
     },
     components: {
-        MuiCssBaseline: {
-            styleOverrides: `
-                @font-face {
-                    font-family: 'Runescape';
-                    font-style: normal;
-                    font-display: swap;
-                    src: local('Runescape'), local('Raleway-Regular'), url(${Runescape}) format('woff2');
-                    unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-                }
-            `,
-        },
         MuiCard: {
             styleOverrides: {
                 root: {
